3cac7f73a761ce0b8ba981f4e8e5228c9e509d99
Stephanie Salzburg <steph_salzburg@hotmail.com>
2025-10-21 23:05:38 -0400

    Created a class that allows for user input for creating a park object

---
+++ GroupExChapter4Ex7Wk7/GroupExChapter4Ex7Wk7/Class1.cs
@@ -0,0 +1,81 @@
+﻿/* 
+ * Programmer: Stephanie Salzburg, Jonquil Terry, David Segura Aba, Nathan Wenson, Cameron Rickel
+ * Date: 10/16/25
+ * Purpose: This program is a motorway class that is used as documentation for highway roads. It allows you to provide details to make the travel much easier. 
+ * Assignment: Chapter 4 - Programming Exercise 3
+ */
+
+
+namespace GroupExChapter4Ex7Wk7
+{
+    internal class ParkInformation
+    {
+        //Getter & Setters
+        public string ParkName { get; set; }
+        public string LocationType { get; set; }
+        public double FacilityFee { get; set; }
+        public  int NumEmployees { get; set; }
+        public int NumVisitors12 { get; set; }
+        public double AnnualBudget { get; set; }
+
+        //Fields
+        public string facilities;
+        public double resultOfAnnualAndVisit;
+        public  double resultOfFee;
+
+
+        //Constructor 
+        public ParkInformation() { 
+        //Empty constructor to create the object 
+        }
+
+        //Construstors with parameters to hold the information to build the object with information.
+        public ParkInformation  ( string ParkName, string LocationType, double FacilityFee, int NumEmployees, int NumVisitors12, double AnnualBudget )
+        {
+            this.ParkName = ParkName;
+            this.LocationType = LocationType;
+            this.FacilityFee = FacilityFee;
+            this.NumEmployees = NumEmployees;
+            this.NumVisitors12 = NumVisitors12;
+            this.AnnualBudget = AnnualBudget;
+
+        }
+
+
+        //String of facilities available at the location
+        public string FacilitiesAvailable()
+        {
+
+            facilities = "Facilities:" + " Walking trails, Jogging trails, Biking trails \n Food Court, Vending Machines";
+            return facilities;
+        }
+         //Method that calculates the cost per visitor
+         public double CostPerVisitor()
+        {
+            resultOfAnnualAndVisit = AnnualBudget / NumVisitors12;
+            return resultOfAnnualAndVisit;
+
+        }
+        //Calculates the money earned from feees
+        public double MoneyFromFee()
+        {
+            resultOfFee = FacilityFee * NumVisitors12;
+            return resultOfFee;
+        }
+
+
+        //Method to string turns all the information input into a string.
+        public override string ToString()
+        {
+            CostPerVisitor(); 
+            MoneyFromFee();    
+
+            return $"Park Name: {ParkName},\nLocation: {LocationType},\nFacilities: {facilities},\nNumber of Employees: {NumEmployees},\nNumber of Visitors: {NumVisitors12},\nAnnual Budget: {AnnualBudget},\nMoney from Fees: {resultOfFee},\nCost per Visitor: {resultOfAnnualAndVisit}";
+        }
+
+
+
+
+
+    }
+}
---
+++ GroupExChapter4Ex7Wk7/GroupExChapter4Ex7Wk7/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+/* 
+ * Programmer: Stephanie Salzburg, Jonquil Terry, David Segura Aba, Nathan Wenson, Cameron Rickel
+ * Date: 10/16/25
+ * Purpose: This program collects information from a user regarding national,local, state parks and creates an object with the information.
+ * Assignment: Chapter 4 - Programming Exercise 7
+ */
+
+
+
+
+
+namespace GroupExChapter4Ex7Wk7
+{
+    internal class Class
+    {
+
+        //Test method to take user input and call the methods to do calculations
+        static void Main(string[] args)
+        {
+            ParkInformation Park = new ParkInformation();  //Park Object
+
+            Console.WriteLine("Park Name:");
+            Park.ParkName = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Park Type (1:State, 2:National, 3:Local):");
+                string typeInput = Console.ReadLine();
+                switch (typeInput)
+                {
+                    case "1":
+                        Park.LocationType = "State";
+                        break;
+                    case "2":
+                        Park.LocationType = "National";
+                        break;
+                    case "3":
+                        Park.LocationType = "Local";
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please enter 1, 2, or 3.");
+                        continue; 
+                }
+                break; 
+            }
+
+
+            Console.WriteLine("Facility Fee:");
+            Park.FacilityFee = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("How many total employees over the last 12 months::");
+            Park.NumEmployees = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Number of Visitors in the last 12 months:");
+            Park.NumVisitors12 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Annual Budget:");
+            Park.AnnualBudget = Convert.ToDouble(Console.ReadLine());
+
+            Park.FacilitiesAvailable();
+           
+            Console.WriteLine("\n--- Park Information ---");
+            Console.WriteLine(Park.ToString());
+            Console.WriteLine("\n------------------------");
+           
+
+        }
+    }
+}
