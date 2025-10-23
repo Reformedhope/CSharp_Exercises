a7e559172f46645b59e93cfdba4a8b862deeec7d
Stephanie Salzburg <steph_salzburg@hotmail.com>
2025-10-22 22:34:09 -0400

    Created an employee class to get the revenue total from sales.

---
+++ EmployeePay/EmployeePay/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePay
+{
+    internal class Employee
+    {
+        //constant 
+        const double TotalRevenueForEmployee = 0.07;
+        const double FederalTaxRate = 0.18;
+        const double RetirementContribution = 0.10;
+        const double SocialSecurity = 0.06;
+
+        //fields
+        public double FederalTaxAmount;
+        public double TakeHome;
+        public double RetirementAmount;
+        public double Commission;
+        public double SocialSecurityAmount;
+
+        //Setters & Getters
+        public string Name { get; set; }
+        public double SalesAmount { get; set; }
+        
+        //constructors
+        public Employee() 
+        { 
+         // empty to generate object
+        }
+        //to build object
+        public Employee(string Name, double SalesAmount) 
+        {
+            this.Name = Name;
+            this.SalesAmount = SalesAmount;
+        }
+
+
+        //Method to calculate fed tax
+        public double GetTaxAmount() 
+        {
+            FederalTaxAmount = Commission * FederalTaxRate;
+                return FederalTaxAmount;
+        }
+        //Method to calculate SS amount
+        public double SocialSecurityWithheld()
+        {
+            SocialSecurityAmount = Commission * SocialSecurity;
+            return SocialSecurityAmount;
+        }
+        //Method to get retirment information
+        public double AdditionalDeductions()
+        {
+            RetirementAmount = Commission * RetirementContribution;
+            return RetirementAmount;
+        }
+        
+       //method to calculate commision amount
+        public double CommisionsIncome()
+        {
+            Commission = SalesAmount * TotalRevenueForEmployee;
+            return Commission;
+        }
+        //Method to get the take home after everything has been deducted. 
+        public double RevenueTotal()
+        {
+            TakeHome = Commission - (FederalTaxAmount + SocialSecurityAmount + RetirementAmount);// Adds the deductions togethe then commisions subtracts from the ammount. 
+            return TakeHome;
+        }
+
+
+    }
+}
---
+++ EmployeePay/EmployeePay/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+///* 
+// * Programmer: Stephanie Salzburg
+// * Date: 10/22/2025
+// * Purpose: Program takes user input for name and sales total. The program then calculates how many federal tax, social security tax, retirement funds, and commision is earned by running through calculations. 
+//              The program then out puts the information to the user and displays. 
+// * Class: CITP 180
+// * Assignment: Chapter 4 - Programming Excercise 8
+// * /
+
+namespace EmployeePay
+{
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //variables 
+            double tempSalesHolder;
+            string enter;
+            
+
+
+           
+            //While loop to allow for more object to be created
+            while (true) 
+            {   
+                
+                Employee E = new Employee();//Creating an object
+
+                Console.WriteLine("Would you like to enter revenue calculations for an employee?(Y or N): ");
+                enter = Console.ReadLine().ToUpper();
+                if (enter == "Y")
+                {
+
+
+                    Console.WriteLine("Employee Name");
+                    E.Name = Console.ReadLine();
+
+
+                    while (true) //while loop to get correct input from user for a double input.
+                    {
+                        Console.WriteLine("Sales amount for the week: ");
+                        string input = Console.ReadLine();
+                        if (double.TryParse(input, out tempSalesHolder))
+                        {
+                            E.SalesAmount = tempSalesHolder;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter a monetary value.\n");
+                        }
+                    }
+                    //calling the methods in the other calls
+                    E.CommisionsIncome();
+                    E.GetTaxAmount();
+                    E.SocialSecurityWithheld();
+                    E.AdditionalDeductions();
+                    E.RevenueTotal();
+
+                    //Displays information well formated 
+                    Console.WriteLine("--Employee Revenue Data---\n");
+                    Console.WriteLine($"Employee Name: {E.Name}\n");
+                    Console.WriteLine($"Total Sales: ${E.SalesAmount:F2}\n");
+                    Console.WriteLine($"Total Tax Deductions: ${E.FederalTaxAmount:F2}\n");
+                    Console.WriteLine($"Retirement Amount: ${E.RetirementAmount:F2}\n");
+                    Console.WriteLine($"Social Security Deductions: ${E.SocialSecurityAmount:F2}\n");
+                    Console.WriteLine($"Take Home Pay: ${E.TakeHome:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("See you again soon.");
+                    break;
+                }
+            }
+
+        }
+    }
+}
