9577b4d22e46ea9b1f8b0244935107b41767b847
Stephanie Salzburg <steph_salzburg@hotmail.com>
2025-11-04 20:43:54 -0500

    created another array application

---
+++ Assignment7Ex10/Assignment7Ex10/CourseReport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace Assignment7Ex10
+{
+    internal class CourseReport 
+    {
+     
+
+        static void Main(string[] args)
+        {
+            //3 parallel arrays
+            string[] className = { "CS150", "CS250", "CS270", "CS300", "CS350" };
+            int[] currentEnrollment = { 180, 21, 9, 4, 20 };
+            int[] maximumEnrollment = { 200, 30, 20, 20, 20 };
+
+            //creating an array of Course objects
+            Course[] courses = new Course[className.Length];
+
+            // Loops through the indexes and create course objects using data from the arrays, store them in the courses array
+            for (int i = 0; i < className.Length; i++)
+            {
+                courses[i] = new Course(className[i], currentEnrollment[i], maximumEnrollment[i]);
+            }
+            //loops over courses array and prints the tostring method in the blue print. 
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i].ToString());
+                Console.WriteLine("_____________");
+            }
+        
+
+            //loops through the arrays to print out the arrays in a column. Added to make it look like the example. 
+            Console.WriteLine($"Course Name: \tCurrently Taken Seats: \tTotal Seats Available: \t Seats Left: ");
+            Console.WriteLine("-------------------------------------------------------------------------------");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{className[i]}\t\t\t{currentEnrollment[i]}\t\t\t{maximumEnrollment[i]}\t\t\t{courses[i].SeatsLeft()}");
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create Two classes name DataMembers and 
---
+++ Assignment7Ex10/Assignment7Ex10/course.cs
@@ -0,0 +1,44 @@
+﻿using System;
+///* 
+// * Programmer:Stephanie Salzburg
+// * Date:11/4/2025
+// * Purpose: A program that creates a blueprint of a course and seats available. Then prints out the information displaying the information from the 3 parallel arrays in two slightly different formats. 
+// * Class: CITP 180
+// * Assignment: Chapter 7 - Programming Excercise 10
+// * /
+
+namespace Assignment7Ex10
+{
+    internal class Course
+    {
+       
+        //Ready only 
+        public string cName { get; }
+        public int current { get; }
+        public int maximum { get;}
+
+        //Constructor
+       public Course(string cName, int current, int maximum)
+        {
+            this.cName = cName;
+            this.current = current;
+            this.maximum = maximum;
+
+        }
+        //calculates the seats left
+        public int SeatsLeft()
+        {    
+
+                return maximum - current;
+
+        }
+
+        //to string method to return an objext as a string
+        public override string ToString()
+        {
+            return $"Course Name: {cName}  Currently Taken Seats: {current} Total Seats Available: {maximum} Seats Left: {SeatsLeft()}";
+        }
+
+
+    }
+}
