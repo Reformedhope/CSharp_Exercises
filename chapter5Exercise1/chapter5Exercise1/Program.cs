2f98dfc5923c9c1754b1b1136f4fff63f03fc00c
Stephanie Salzburg <steph_salzburg@hotmail.com>
2025-11-17 21:26:56 -0500

    started another fun project

---
+++ chapter5Exercise1/chapter5Exercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter5Exercise1
+{
+    internal class Program
+    {
+
+        private int dec;
+
+         private void UserInputsDecimal()
+        {
+            
+            
+             Console.WriteLine("Please enter a decimal to convert to a roman numeral: ");
+             int.TryParse(Console.ReadLine(), out dec);
+           
+                
+            
+            
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
