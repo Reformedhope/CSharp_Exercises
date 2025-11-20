0a40e9f16c3d22d8fa40a276ed55c7d5c1f2c675
Stephanie Salzburg <steph_salzburg@hotmail.com>
2025-11-19 20:13:53 -0500

    completed a WAF

---
+++ GroupEX10Wk11/GroupEX10Wk11/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace GroupEX10Wk11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSubmitNumber = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtNumberBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmitNumber
+            // 
+            this.btnSubmitNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmitNumber.Location = new System.Drawing.Point(217, 259);
+            this.btnSubmitNumber.Name = "btnSubmitNumber";
+            this.btnSubmitNumber.Size = new System.Drawing.Size(149, 55);
+            this.btnSubmitNumber.TabIndex = 0;
+            this.btnSubmitNumber.Text = "Submit Number";
+            this.btnSubmitNumber.UseVisualStyleBackColor = true;
+            this.btnSubmitNumber.Click += new System.EventHandler(this.btnSubmitNumber_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.IndianRed;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnReset.Location = new System.Drawing.Point(415, 259);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(143, 56);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(147, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(600, 112);
+            this.lblInstructions.TabIndex = 2;
+            this.lblInstructions.Text = resources.GetString("lblInstructions.Text");
+            this.lblInstructions.Click += new System.EventHandler(this.lblInstructions_Click);
+            // 
+            // txtNumberBox
+            // 
+            this.txtNumberBox.Location = new System.Drawing.Point(405, 158);
+            this.txtNumberBox.Name = "txtNumberBox";
+            this.txtNumberBox.Size = new System.Drawing.Size(112, 20);
+            this.txtNumberBox.TabIndex = 3;
+            this.txtNumberBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(230, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your number guess here: ";
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Location = new System.Drawing.Point(563, 161);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(106, 13);
+            this.lblAttempts.TabIndex = 5;
+            this.lblAttempts.Text = "Number of Attempts: ";
+            this.lblAttempts.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumberBox);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSubmitNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmitNumber;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtNumberBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblAttempts;
+    }
+}
+
---
+++ GroupEX10Wk11/GroupEX10Wk11/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+
+/*
+ * Programmers: Cameron Rickel, Stephanie Salzburg, Jonquil Terry, Nathan Wenson, David Segura Aba
+ * Date: 11/19/25
+ * Purpose: The purpose of this application is to be anumber guessing game. 
+ * The user enters a number and the background changes color based on if the number is too high or too low. 
+ * Class : CITP 180
+ * Assignment: Chapter 9 - Programming Exercise 10
+ */
+
+namespace GroupEX10Wk11
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        int target;
+        int numberEntered;
+        int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            target = random.Next(0, 101);
+            this.AcceptButton = btnSubmitNumber;
+            this.CancelButton = btnReset;
+        }
+
+        private void lblInstructions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnSubmitNumber_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumberBox.Text) ||
+                !int.TryParse(txtNumberBox.Text, out numberEntered))
+            {
+                MessageBox.Show("Invalid value");
+                this.BackColor = Color.Orange;
+                return;
+            }
+            if (numberEntered == target)
+            {
+
+                this.BackColor = Color.Green;
+                MessageBox.Show("You got it!");
+            }
+            else if (numberEntered > target)
+            {
+                //MessageBox.Show("Too High! Try again!");
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                //MessageBox.Show("Too Low! Try again!");
+                this.BackColor = Color.Blue;
+            }
+            if (numberEntered != target)
+            {
+                counter++;
+                lblAttempts.Text = $"Number Attempts:{counter}";
+
+            }
+            txtNumberBox.Clear();
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtNumberBox.Clear();
+            this.BackColor = Color.White;
+            counter = 0;
+            lblAttempts.Text = $"Number Attempts:{counter}";
+            target = random.Next(0, 101);
+        }
+    }
+}
